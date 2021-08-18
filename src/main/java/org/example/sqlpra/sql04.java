fc3784c731343e53ea1bcb75303160f7590b7f82
boy_kai@126.com <boy_kai@126.com>
2021-08-18 16:46:36 +0800

    'flinksql练习'

--- src/main/java/org/example/sqlpra/sql04.java
+++ src/main/java/org/example/sqlpra/sql04.java
@@ -1,6 +1,7 @@
 package org.example.sqlpra;
 
 import org.apache.flink.table.api.EnvironmentSettings;
+import org.apache.flink.table.api.Table;
 import org.apache.flink.table.api.TableEnvironment;
 
 /**
@@ -10,6 +11,10 @@ public class sql04 {
     public static void main(String[] args) {
         EnvironmentSettings batchSet = EnvironmentSettings.newInstance().useBlinkPlanner().inBatchMode().build();
         TableEnvironment batchTableEnv = TableEnvironment.create(batchSet);
+        Table table = batchTableEnv.fromValues(1, 2, 3);
 
+        batchTableEnv.createTemporaryView("num",table);
+
+        Table tableQ = batchTableEnv.sqlQuery("select * from num");
     }
 }
