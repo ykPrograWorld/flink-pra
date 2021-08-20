bf7154027dfc4529c35ce6b800116c441e0014e3
boy_kai@126.com <boy_kai@126.com>
2021-08-20 13:40:14 +0800

    '添加选择排序'

--- src/main/java/org/example/datastru/LeetCodePra.java
+++ src/main/java/org/example/datastru/LeetCodePra.java
@@ -10,6 +10,12 @@ public class LeetCodePra {
         int[] arr = {1, 3, 5, 7, 9, 12, 14};
         System.out.println(midFind(arr, 9));
         System.out.println(midFindPra(arr, 9));
+
+
+        int[] arrSele = {3, 1, 5, 14, 7, 12, 9};
+        selectSort(arrSele);
+        System.out.println();
+        selectSortPra(arrSele);
     }
 
     /*
@@ -55,24 +61,64 @@ public class LeetCodePra {
 
     /*
     选择排序
+    从未排序中永远挑最小的数和当前值替换
      */
     public static void selectSort(int[] arr){
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minPos = i;
             for (int j = i + 1; j < arr.length; j++) {
-                if(arr[i] > arr[j]){
-                    swap(i,j,arr);
-                }
+                minPos = arr[j] < arr[minPos] ? j : minPos;
             }
-
+            swap(i,minPos,arr);
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
         }
     }
+
     /*
     位置替换  i和j互换
      */
     public static void swap(int i, int j, int[] arr) {
-        int temp;
-        temp = arr[i];
+        int temp = arr[i];
         arr[i] = arr[j];
-        arr[j] = arr[i];
+        arr[j] = temp;
     }
+
+    /*
+    选择排序  self pra
+     */
+    public static void selectSortPra(int[] arr){
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minPos = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                minPos = arr[j] < arr[i] ? j : minPos;
+            }
+            swap(arr,i,minPos);
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    /*
+    交换
+     */
+    public static void swap(int[] arr,int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+
+
+
+
+
+
+
+
+
+
 }
