3b8d39a7d583c520571db4c0ed0c676aa514f25c
boy_kai@126.com <boy_kai@126.com>
2021-09-23 13:48:58 +0800

    添加flink连接kafka代码和依赖

--- src/main/java/org/example/utils/FlinkUtils.java
+++ src/main/java/org/example/utils/FlinkUtils.java
@@ -1,7 +1,11 @@
 package org.example.utils;
 
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+
+import java.util.Properties;
 
 public class FlinkUtils {
 
@@ -13,10 +17,22 @@ public class FlinkUtils {
         return wStream;
     }
 
+    public static DataStreamSource getKafkaStreamSource() throws Exception{
+        StreamExecutionEnvironment env = getStreamEnv();
+        String topic = "";
+        Properties pro = new Properties();
+        FlinkKafkaConsumer flinkKafkaConsumer = new FlinkKafkaConsumer<>(topic, SimpleStringSchema.class.newInstance(),pro);
+        DataStreamSource streamSource = env.addSource(flinkKafkaConsumer);
+        return streamSource;
+    }
+
     public static StreamExecutionEnvironment getStreamEnv(){
         if(env == null){
             env = StreamExecutionEnvironment.getExecutionEnvironment();
         }
         return env;
     }
+
+
+
 }
